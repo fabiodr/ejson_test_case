05545f63d1a88eab6c855775e5352d4b403e1335
Toms Mikoss <toms.mikoss@gmail.com>
2015-11-20 19:23:28 +0200

    Init

---
+++ client/client.js
@@ -0,0 +1,56 @@
+Session.setDefault('shouldRender', false);
+Session.setDefault('count', 10);
+Session.setDefault('complexity', 1);
+Session.setDefault('depth', 0);
+Session.setDefault('lastSize', 0);
+
+const settingsCallback = (err, { count, complexity, depth }) => {
+  if(err) {
+    console.error(err);
+  } else {
+    Session.set('count', count);
+    Session.set('complexity', complexity);
+    Session.set('depth', depth);
+  }
+};
+
+Meteor.startup(() => {
+  Meteor.call('count', Session.get('count'), settingsCallback); // Re-sync with server settings
+
+  Meteor.setInterval(() => {
+    Meteor.call('lastSize', (err, lastSize) => {
+      Session.set('lastSize', lastSize.toLocaleString());
+    });
+  }, 1000);
+});
+
+
+Template.ModeSelector.helpers({
+  shouldRender: (value) => {
+    return Session.get('shouldRender') === value;
+  },
+  session: (key) => {
+    return Session.get(key)
+  }
+});
+
+Template.ModeSelector.events({
+  "change input[type='radio']": function({ target: { value } }) {
+    Session.set('shouldRender', value === 'true');
+  },
+  "change input[type='range']": function({ target: { name, value } }) {
+    const intValue = parseInt(value, 10);
+    Meteor.call(name, intValue, settingsCallback);
+  }
+});
+
+Template.Data.helpers({
+  data: function(){
+    return DataCollection.find({}, { sort: { updatedAt: -1 }});
+  }
+});
+Template.DataEntry.helpers({
+  stringified: function() {
+    return JSON.stringify(_.omit(this, 'children'));
+  }
+});
---
+++ server/server.js
@@ -0,0 +1,76 @@
+var faker = Meteor.npmRequire('Faker');
+
+const options = {
+  count: 10,
+  complexity: 1,
+  depth: 0
+};
+
+const recordIds = [];
+
+function buildRecord(complexity, depth) {
+  var record = {};
+
+  _.times(complexity, (i) => {
+    record["boolean_"+i] = _.sample([true, false]);
+    record["number_"+i] = faker.random.number(100000);
+    record["date_"+i] = faker.Date.recent(30);
+  });
+
+  if (depth > 0) {
+    record.children = [];
+
+    _.times(depth * 2, () => {
+      record.children.push( buildRecord(Math.floor(complexity/2), depth-1));
+    });
+  }
+
+  return record;
+}
+
+function resetCollection() {
+  DataCollection.remove({});
+
+  recordIds.length = 0;
+
+  _.times(options.count, () => {
+    const newAttributes = buildRecord(options.complexity, options.depth);
+    recordIds.push(DataCollection.insert( _.extend({ updatedAt: new Date() }, newAttributes)));
+  });
+}
+
+const checkNum = Match.Where((x) => {
+  check(x, Match.Integer);
+  return x >= 0;
+});
+
+Meteor.methods({
+  count: (count) => {
+    check(count, checkNum);
+    options.count = count;
+    resetCollection();
+    return options;
+  },
+  complexity: (complexity) => {
+    check(complexity, checkNum);
+    options.complexity = complexity;
+    return options;
+  },
+  depth: (depth) => {
+    check(depth, checkNum);
+    options.depth = depth;
+    return options;
+  },
+  lastSize: () => {
+    return JSON.stringify(DataCollection.findOne({}, { sort: { updatedAt: -1 }})).length;
+  }
+});
+
+Meteor.startup(() => {
+  resetCollection();
+
+  Meteor.setInterval(() => {
+    const newAttributes = buildRecord(options.complexity, options.depth);
+    DataCollection.update(_.sample(recordIds), _.extend({ updatedAt: new Date() }, newAttributes));
+  }, 1000);
+});
